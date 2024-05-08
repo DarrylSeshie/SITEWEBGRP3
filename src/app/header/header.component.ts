a9bb757187176e7d96d5f864cb1653d424fa3efa
DarrylSeshie <127939267+DarrylSeshie@users.noreply.github.com>
2024-05-09 01:07:23 +0200

    Ajout debut token sur user connect et affichage des gestion ds le header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  connectedUser: User | null = null; // Utilisateur connecté (initialisé à null par défaut)
 
+  constructor(/*private authService: AuthService,*/ private userService: UserService) {}
+
+  ngOnInit(): void {
+  //  const token = this.authService.getToken();
+
+   /* if (token) {
+      const decodedToken: any = this.authService.getUserFromToken(token); // Décode le token pour obtenir l'identifiant de l'utilisateur methode ds le service des authentifics
+      const userId = decodedToken.userId;
+
+      this.userService.getUserById(userId).subscribe(
+        (user: User) => {
+          this.connectedUser = user;
+        },
+        (error) => {
+          console.error('Erreur lors de la récupération des informations de l\'utilisateur :', error);
+        }
+      );
+    }*/
+  }
+/* pret a deco en fcn des nom des methodes
+  logout(): void {
+    this.authService.removeToken();
+    this.connectedUser = null; // Réinitialiser l'utilisateur connecté
+    // Rediriger l'utilisateur vers la page de connexion ou autre action
+  }*/
 }
