a2dfdc66b2ce43394b4d80605f8f13ab4802e509
DarrylSeshie <127939267+DarrylSeshie@users.noreply.github.com>
2024-05-23 22:51:30 +0200

    Corr menu +dec + co ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/user.service';
 import { Observable } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 
 
@@ -11,13 +13,35 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
-  isLoggedIn$ = this.service.isLoggedIn;
-  
-  constructor(public service: UserService) {  }
+  isLoggedIn$: Observable<boolean>;
 
-  ngOnInit(): void {}
-
+  constructor(private service: UserService, private router: Router) {
+    this.isLoggedIn$ = this.service.isLoggedIn;
+  }
  
+
+  ngOnInit(): void {
+    this.isLoggedIn$.subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.router.events.pipe(
+          filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+        ).subscribe((event: NavigationEnd) => {
+          if (event.url === '/' || event.url.includes('/connexion')) {
+            this.router.navigate(['/acceuil']);
+          }
+        });
+      } else {
+        if (!localStorage.getItem('logout')) {
+          localStorage.setItem('logout', 'true');
+          this.router.navigate(['/']).then(() => {
+            window.location.reload(); // Recharge l'onglet après la redirection
+          });
+        } else {
+          localStorage.removeItem('logout');
+        }
+      }
+    });
+  }
 
   
   
